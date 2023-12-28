b2136b42aaad0ec2d6d329bc0927bf7b5155a44d
GAURAV CHAUDHARY <137998824+ANAMASGARD@users.noreply.github.com>
2023-12-28 10:18:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+
+const material = new THREE.MeshBasicMaterial({color : 0xffd700})
+const mesh = new THREE.Mesh(geometry , material)
+scene.add(mesh)
+
+const sizes = {
+    width: 900,
+    height: 600
+}
+const camera = new THREE.PerspectiveCamera(79 , sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = -1
+camera.position.y = -1
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl')
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width , sizes.height)
+renderer.render(scene , camera)
+
+
